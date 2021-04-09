8c182b85445da78cec7c7ce5d1397f6c90a7bbdb
Yassine MALTI <yassine.malti@gmail.com>
2021-04-09 10:49:40 +0100

    Update

---
+++ data.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS 'datatest';
+CREATE DATABASE IF NOT EXISTS 'datatest';
+USE 'datatest';
+
+/*
+---------------------------------------------------
+-- Création des tables ----------------------------
+---------------------------------------------------
+*/
+
+CREATE TABLE IF NOT EXISTS 'ninjatable'
+{
+	'id_ninja' INT NOT NULL AUTO_INCREMENT,
+	'ninja_firstname' VARCHAR(30) NOT NULL UNIQUE,
+	'ninja_lastname' VARCHAR(30) NOT NULL,
+	PRIMARY KEY('id_ninja')
+};
+
+/*
+---------------------------------------------------
+-- Ajout de quelques enregistrements --------------
+---------------------------------------------------
+*/
+
+INSERT INTO 'ninjatable'('ninja_firstname', 'ninja_lastname')
+VALUES
+('Naruto', 'UZUMAKI'),
+('Kakashi', 'HATAKE'),
+('Sakura', 'HARUNO'),
+('Sasuke', 'UCHIHA'),
+('Hinata', 'HYUDA');
